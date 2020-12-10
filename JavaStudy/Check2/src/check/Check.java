c9e6af574dca9884bf480e944fd8a8ec46b069a5
masaki915 <msk91523@gmail.com>
2020-12-11 00:06:04 +0900

    テスト提出
    2章テストの提出

---
+++ JavaStudy/Check2/src/check/Check.java
@@ -0,0 +1,30 @@
+package check;
+
+import constants.Constants;
+
+public class Check {
+    private String firstName = "花岡";
+    private String lastName = "柾";
+
+    private void printName() {
+        System.out.println("printNameメソッド → " + firstName + lastName);
+    }
+
+    public static void main(String []args){
+        Check check = new Check();
+        check.printName();
+
+        String name = Constants.CHECK_CLASS_JAVA;
+        String masterName = Constants.CHECK_CLASS_HOGE;
+        Pet pet = new Pet(name, masterName);
+        pet.introduce();
+
+        RobotPet robotPet = (RobotPet) new Pet(name, masterName);
+        name = subName;
+        masterName = subMasterName;
+        String subName = Constants.CHECK_CLASS_R2D2;
+        String subMasterName = Constants.CHECK_CLASS_LUKE;
+        robotPet.introduce();
+    }
+
+}
